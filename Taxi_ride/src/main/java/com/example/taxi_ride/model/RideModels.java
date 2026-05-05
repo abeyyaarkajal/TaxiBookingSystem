6f25bfdd7b3d9fe6b0b3e099b05a4de5a91d7f22
Kanishka Agarwal <kanishkaagarwal201@gmail.com>
2026-05-05 14:44:40 +0530

    added ride models

--- Taxi_ride/src/main/java/com/example/taxi_ride/model/RideModels.java
+++ Taxi_ride/src/main/java/com/example/taxi_ride/model/RideModels.java
@@ -1,4 +1,23 @@
-package com.example.taxi_ride.model;
 
-public class RideModels {
+package com.example.taxi_ride.model; // Updated package name
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+class Location {
+    private double lat;
+    private double lng;
+}
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class RideModels { // Changed class name to match file
+    private String id;
+    private String riderName;
+    private Location pickup;
+    private Location drop;
+    private double fare;
+    private String status; 
+    private String driverId;
 }
